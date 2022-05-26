77b418aea20a86cbbbe6939b6ec9855a1aa4b027
Eurides Bonecher Filho <euridesb@hotmail.com>
2022-05-26 17:04:36 -0300

    att aula EAD python 26/05

--- tratamento_excecoes.py
+++ tratamento_excecoes.py
@@ -1,4 +1,22 @@
+# try:
+#     print(2/0)
+# except:
+#     print('Erro: Exceção Gerada.')
+
+
+# try:
+#     print(2/0)
+# except ZeroDivisionError as erro:
+#     print('Impossivel dividir por zero [' +str(erro) +']')
+# except NameError as mensagem:
+#     print('Variavel não declarada [' + str(mensagem) + ']')
+# finally:
+#     print('Operação efetuada com sucesso.')
+
 try:
-    print(2/0)
-except:
-    print('Erro: Exceção Gerada.')
+    print('Carregando Operação')
+    y = 0
+    for x in range(0,632):
+        y = y + 1
+finally:
+    print('Operação finalizada com sucesso')
