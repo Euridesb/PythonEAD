3d839d945404d0c17edabf514f386d07f9c1aa51
Eurides Bonecher Filho <euridesb@hotmail.com>
2022-05-31 19:01:37 -0300

    Curso EAD Python

---
+++ Python_Avancado/POO/testes.py
@@ -0,0 +1,24 @@
+
+
+def comparaValores(v1,v2):
+    if(v1!=v2):
+        v1=v2
+    assert(v1==v2)
+
+def ObtemEscola(escola):
+    if(escola!='Proway'):
+        escola = 'Proway'
+    assert(escola == 'Proway')
+
+def ValidaIdade(ano):
+    idade = 2022 - ano
+    assert(idade >=18)
+
+def ValidaSoma(v1,v2):
+    soma = v1 + v2
+    assert(soma>=10)
+
+ValidaSoma(5,6)
+ValidaIdade(2002)
+ObtemEscola('Nome da escola')
+comparaValores(30,25)
---
+++ Python_Avancado/TESTE/teste_.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class Teste(unittest.TestCase()):
+
+    def test_A(vl1,vl2):
+        assert(vl1 == vl2)
